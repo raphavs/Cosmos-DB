99bbcb6b6f71806a76ffbbc912627356bc206fdf
raphavs <raphael.vosseler@htwg-konstanz.de>
2019-11-17 23:20:07 +0100

    Implement exemplary functionalities

--- Cosmos-DB/Program.cs
+++ Cosmos-DB/Program.cs
@@ -1,19 +1,17 @@
 ﻿using System;
 using System.Threading.Tasks;
-using System.Collections.Generic;
-using System.IO;
 using System.Net;
+using Cosmos_DB.Object;
 using Microsoft.Azure.Cosmos;
-using Newtonsoft.Json;
 
 namespace Cosmos_DB
 {
-    class Program
+    internal class Program
     {
         // The Azure Cosmos DB endpoint for running this sample.
-        private static readonly string EndpointUri = "https://rapha.documents.azure.com:443/";
+        private const string ENDPOINT_URI = "https://rapha.documents.azure.com:443/";
         // The primary key for the Azure Cosmos account.
-        private static readonly string PrimaryKey = "VJPr6SiLxGNJwt5Qf981U2xMn98HbRZefqWRdwRSzcLRQw1SGZ12uHXzAmSIkre9hKdfTltKf6sZOYly2krguA==";
+        private const string PRIMARY_KEY = "VJPr6SiLxGNJwt5Qf981U2xMn98HbRZefqWRdwRSzcLRQw1SGZ12uHXzAmSIkre9hKdfTltKf6sZOYly2krguA==";
 
         // The Cosmos client instance
         private CosmosClient cosmosClient;
@@ -21,28 +19,29 @@ namespace Cosmos_DB
         // The database we will create
         private Database database;
 
-        // The container we will create.
-        private Container container;
+        // The containers we will create
+        private Container customerContainer;
+        private Container apartmentContainer;
+        private Container reservationContainer;
 
-        // The name of the database and container we will create
-        private string databaseId = "HolidayDatabase";
-        private string containerId = "Test";
-
-
+        // The name of the database and containers we will create
+        private const string DATABASE_ID = "HolidayDatabase";
+        private const string CUSTOMER_CONTAINER_ID = "Customer";
+        private const string APARTMENT_CONTAINER_ID = "Apartment";
+        private const string RESERVATION_CONTAINER_ID = "Reservation";
+        
         public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
             try
             {
                 Console.WriteLine("Beginning operations...\n");
-                Program p = new Program();
+                var p = new Program();
                 await p.GetStartedDemoAsync();
 
             }
             catch (CosmosException de)
             {
-                Exception baseException = de.GetBaseException();
+                var baseException = de.GetBaseException();
                 Console.WriteLine("{0} error occurred: {1}", de.StatusCode, de);
             }
             catch (Exception e)
@@ -59,66 +58,21 @@ namespace Cosmos_DB
         /// <summary>
         /// Entry point to call methods that operate on Azure Cosmos DB resources in this sample
         /// </summary>
-        public async Task GetStartedDemoAsync()
+        private async Task GetStartedDemoAsync()
         {
             // Create a new instance of the Cosmos Client
-            this.cosmosClient = new CosmosClient(EndpointUri, PrimaryKey);
+            this.cosmosClient = new CosmosClient(ENDPOINT_URI, PRIMARY_KEY);
             await this.CreateDatabaseAsync();
             await this.CreateContainerAsync();
-            await this.QueryItemsAsync();
-            
-            // await this.DeletePersonItemAsync();
-            
-            await this.InsertPersonItemAsync();
-            await this.QueryItemsAsync();
-            
-            await this.InsertPersonItemAsync_2();
-            await this.QueryItemsAsync();
+            await this.QueryCustomerItemsAsync();
+            await this.InsertCustomerItemAsync();
+            await this.QueryCustomerItemsAsync();
+            await this.DeleteCustomerItemAsync(); 
+            await this.QueryCustomerItemsAsync();
 
-            /* 
-            await this.AddItemsToContainerAsync();
-            await this.AddSample();
+            /*
             await this.ReplaceFamilyItemAsync();
             */
-        }
-
-        private async Task AddSample()
-        {
-            List<Person> persons = LoadJson();
-
-            foreach (var person in persons)
-            {
-                Console.WriteLine(person.id);
-                Console.WriteLine(person.name);
-                Console.WriteLine(person.passion);
-                
-                
-                try
-                {
-                    // Read the item to see if it exists
-                    ItemResponse<Person> personResponse = await this.container.ReadItemAsync<Person>(person.id, new PartitionKey(person.name));
-                    Console.WriteLine("Item in database with id: {0} already exists\n", personResponse.Resource.id);
-                }
-                catch (CosmosException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
-                {
-                    // Create an item in the container representing the Wakefield family. Note we provide the value of the partition key for this item, which is "Wakefield"
-                    ItemResponse<Person> personResponse = await this.container.CreateItemAsync<Person>(person, new PartitionKey(person.name));
-
-                    // Note that after creating the item, we can access the body of the item with the Resource property off the ItemResponse. We can also access the RequestCharge property to see the amount of RUs consumed on this request.
-                    Console.WriteLine("Created item in database with id: {0} Operation consumed {1} RUs.\n", personResponse.Resource.id, personResponse.RequestCharge);
-                }
-            }
-        }
-
-        private async Task ReadSample()
-        {
-            
-        }
-        
-        private List<Person> LoadJson()
-        {
-            var jsonText = File.ReadAllText("C:/Users/Rapha/source/repos/Cosmos-DB/Cosmos-DB/sample.json");
-            return JsonConvert.DeserializeObject<List<Person>>(jsonText);
         }
 
         /// <summary>
@@ -127,156 +81,56 @@ namespace Cosmos_DB
         private async Task CreateDatabaseAsync()
         {
             // Create a new database
-            this.database = await this.cosmosClient.CreateDatabaseIfNotExistsAsync(databaseId);
+            this.database = await this.cosmosClient.CreateDatabaseIfNotExistsAsync(DATABASE_ID);
             Console.WriteLine("Created Database: {0}\n", this.database.Id);
         }
 
         /// <summary>
-        /// Create the container if it does not exist. 
-        /// Specifiy "/LastName" as the partition key since we're storing family information, to ensure good distribution of requests and storage.
+        /// Create the containers if they do not exist. 
+        /// Specify "/country" as the partition key to ensure good distribution of requests and storage.
         /// </summary>
         /// <returns></returns>
         private async Task CreateContainerAsync()
         {
-            // Create a new container
-            this.container = await this.database.CreateContainerIfNotExistsAsync(containerId, "/name");
-            Console.WriteLine("Created Container: {0}\n", this.container.Id);
-        }
-
-        /// <summary>
-        /// Add Family items to the container
-        /// </summary>
-        private async Task AddItemsToContainerAsync()
-        {
-            // Create a family object for the Andersen family
-            Family andersenFamily = new Family
-            {
-                Id = "Andersen.1",
-                LastName = "Andersen",
-                Parents = new Parent[]
-                {
-            new Parent { FirstName = "Thomas" },
-            new Parent { FirstName = "Mary Kay" }
-                },
-                Children = new Child[]
-                {
-            new Child
-            {
-                FirstName = "Henriette Thaulow",
-                Gender = "female",
-                Grade = 5,
-                Pets = new Pet[]
-                {
-                    new Pet { GivenName = "Fluffy" }
-                }
-            }
-                },
-                Address = new Address { State = "WA", County = "King", City = "Seattle" },
-                IsRegistered = false
-            };
-
-            try
-            {
-                // Read the item to see if it exists.  
-                ItemResponse<Family> andersenFamilyResponse = await this.container.ReadItemAsync<Family>(andersenFamily.Id, new PartitionKey(andersenFamily.LastName));
-                Console.WriteLine("Item in database with id: {0} already exists\n", andersenFamilyResponse.Resource.Id);
-            }
-            catch (CosmosException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
-            {
-                // Create an item in the container representing the Andersen family. Note we provide the value of the partition key for this item, which is "Andersen"
-                ItemResponse<Family> andersenFamilyResponse = await this.container.CreateItemAsync<Family>(andersenFamily, new PartitionKey(andersenFamily.LastName));
-
-                // Note that after creating the item, we can access the body of the item with the Resource property off the ItemResponse. We can also access the RequestCharge property to see the amount of RUs consumed on this request.
-                Console.WriteLine("Created item in database with id: {0} Operation consumed {1} RUs.\n", andersenFamilyResponse.Resource.Id, andersenFamilyResponse.RequestCharge);
-            }
-
-            // Create a family object for the Wakefield family
-            Family wakefieldFamily = new Family
-            {
-                Id = "Wakefield.7",
-                LastName = "Wakefield",
-                Parents = new Parent[]
-                {
-            new Parent { FamilyName = "Wakefield", FirstName = "Robin" },
-            new Parent { FamilyName = "Miller", FirstName = "Ben" }
-                },
-                Children = new Child[]
-                {
-            new Child
-            {
-                FamilyName = "Merriam",
-                FirstName = "Jesse",
-                Gender = "female",
-                Grade = 8,
-                Pets = new Pet[]
-                {
-                    new Pet { GivenName = "Goofy" },
-                    new Pet { GivenName = "Shadow" }
-                }
-            },
-            new Child
-            {
-                FamilyName = "Miller",
-                FirstName = "Lisa",
-                Gender = "female",
-                Grade = 1
-            }
-                },
-                Address = new Address { State = "NY", County = "Manhattan", City = "NY" },
-                IsRegistered = true
-            };
-
-            try
-            {
-                // Read the item to see if it exists
-                ItemResponse<Family> wakefieldFamilyResponse = await this.container.ReadItemAsync<Family>(wakefieldFamily.Id, new PartitionKey(wakefieldFamily.LastName));
-                Console.WriteLine("Item in database with id: {0} already exists\n", wakefieldFamilyResponse.Resource.Id);
-            }
-            catch (CosmosException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
-            {
-                // Create an item in the container representing the Wakefield family. Note we provide the value of the partition key for this item, which is "Wakefield"
-                ItemResponse<Family> wakefieldFamilyResponse = await this.container.CreateItemAsync<Family>(wakefieldFamily, new PartitionKey(wakefieldFamily.LastName));
-
-                // Note that after creating the item, we can access the body of the item with the Resource property off the ItemResponse. We can also access the RequestCharge property to see the amount of RUs consumed on this request.
-                Console.WriteLine("Created item in database with id: {0} Operation consumed {1} RUs.\n", wakefieldFamilyResponse.Resource.Id, wakefieldFamilyResponse.RequestCharge);
-            }
+            // Create container for customers
+            this.customerContainer = await this.database.CreateContainerIfNotExistsAsync(CUSTOMER_CONTAINER_ID, "/country");
+            Console.WriteLine("Created Container: {0}\n", this.customerContainer.Id);
+            
+            // Create container for apartments
+            this.apartmentContainer = await this.database.CreateContainerIfNotExistsAsync(APARTMENT_CONTAINER_ID, "/country");
+            Console.WriteLine("Created Container: {0}\n", this.apartmentContainer.Id);
+            
+            // Create container for reservations
+            this.reservationContainer = await this.database.CreateContainerIfNotExistsAsync(RESERVATION_CONTAINER_ID, "/type");
+            Console.WriteLine("Created Container: {0}\n", this.reservationContainer.Id);
         }
 
         /// <summary>
         /// Run a query (using Azure Cosmos DB SQL syntax) against the container
         /// </summary>
-        private async Task QueryItemsAsync()
+        private async Task QueryCustomerItemsAsync()
         {
-            var sqlQueryText = "SELECT * FROM c";
-
+            const string sqlQueryText = "SELECT * FROM c";
             Console.WriteLine("Running query: {0}\n", sqlQueryText);
 
-            QueryDefinition queryDefinition = new QueryDefinition(sqlQueryText);
-            
-            Console.WriteLine("Query Definition: " + queryDefinition);
-            Console.WriteLine("Query Definition Query Text: " + queryDefinition.QueryText);
-            
-            FeedIterator<Person> queryResultSetIterator = this.container.GetItemQueryIterator<Person>(queryDefinition);
-            
-            Console.WriteLine("Query Result Set Iterator: " + queryResultSetIterator);
-
-            List<Person> persons = new List<Person>();
+            var queryDefinition = new QueryDefinition(sqlQueryText);
+            var queryResultSetIterator = this.customerContainer.GetItemQueryIterator<Customer>(queryDefinition);
 
             while (queryResultSetIterator.HasMoreResults)
             {
-                FeedResponse<Person> currentResultSet = await queryResultSetIterator.ReadNextAsync();
-                foreach (Person person in currentResultSet)
+                var currentResultSet = await queryResultSetIterator.ReadNextAsync();
+                foreach (var customer in currentResultSet)
                 {
+                    Console.WriteLine(customer.id);
+                    Console.WriteLine(customer.firstname + " " + customer.lastname);
+                    Console.WriteLine(customer.email);
                     Console.WriteLine();
-                    persons.Add(person);
-                    Console.WriteLine("\tRead {0}\n", person);
-                    Console.WriteLine("Person Nr. " + person.id);
-                    Console.WriteLine("Name: " + person.name);
-                    Console.WriteLine("Passion: " + person.passion);
                 }
             }
         }
 
+        /*
+        ++++++ UPDATE ITEM: ++++++
         /// <summary>
         /// Replace an item in the container
         /// </summary>
@@ -294,62 +148,56 @@ namespace Cosmos_DB
             wakefieldFamilyResponse = await this.container.ReplaceItemAsync<Family>(itemBody, itemBody.Id, new PartitionKey(itemBody.LastName));
             Console.WriteLine("Updated Family [{0},{1}].\n \tBody is now: {2}\n", itemBody.LastName, itemBody.Id, wakefieldFamilyResponse.Resource);
         }
+        */
 
-        /// <summary>
-        /// Delete an item in the container
-        /// </summary>
-        private async Task DeletePersonItemAsync()
-        {
-            var personId = "2";
-            var partitionKeyValue = "Dominik";
-
-            // Delete an item. Note we must provide the partition key value and id of the item to delete
-            ItemResponse<Person> personResponse = await this.container.DeleteItemAsync<Person>(personId, new PartitionKey(partitionKeyValue));
-            Console.WriteLine("Deleted Person [{0},{1}]\n", personId, partitionKeyValue);
-        }
-        
         /// <summary>
         /// Insert an item in the container
         /// </summary>
-        private async Task InsertPersonItemAsync()
+        private async Task InsertCustomerItemAsync()
         {
-            var rene = new Person
+            var customer = new Customer
             {
-                id = "4",
-                name = "Rene",
-                passion = "Coden"
+                id = "78",
+                firstname = "Rene",
+                lastname = "Borner",
+                date_of_birth = "30.12.1998",
+                email = "mail@reneborner.de",
+                phone = "123456789",
+                street = "Highway 42",
+                city = "Constance",
+                plz = "78467",
+                country = "Germany",
+                blz = "1234",
+                bank_account_number = "1234567"
             };
 
             try
             {
                 // Read the item to see if it exists
-                ItemResponse<Person> personResponse = await this.container.ReadItemAsync<Person>(rene.id, new PartitionKey(rene.name));
-                Console.WriteLine("Item in database with id: {0} already exists\n", personResponse.Resource.id);
+                var customerResponse = await this.customerContainer.ReadItemAsync<Customer>(customer.id, new PartitionKey(customer.country));
+                Console.WriteLine("Item in database with id: {0} already exists\n", customerResponse.Resource.id);
             }
             catch (CosmosException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
             {
-                // Create an item in the container representing the Wakefield family. Note we provide the value of the partition key for this item, which is "Wakefield"
-                ItemResponse<Person> personResponse = await this.container.CreateItemAsync<Person>(rene, new PartitionKey(rene.name));
+                // Create an item in the container representing the Rene customer. Note we provide the value of the partition key for this item, which is "Germany"
+                var customerResponse = await this.customerContainer.CreateItemAsync<Customer>(customer, new PartitionKey(customer.country));
 
                 // Note that after creating the item, we can access the body of the item with the Resource property off the ItemResponse. We can also access the RequestCharge property to see the amount of RUs consumed on this request.
-                Console.WriteLine("Created item in database with id: {0} Operation consumed {1} RUs.\n", personResponse.Resource.id, personResponse.RequestCharge);
+                Console.WriteLine("Created item in database with id: {0} Operation consumed {1} RUs.\n", customerResponse.Resource.id, customerResponse.RequestCharge);
             }
         }
         
         /// <summary>
-        /// Insert an item in the container
+        /// Delete an item in the container
         /// </summary>
-        private async Task InsertPersonItemAsync_2()
+        private async Task DeleteCustomerItemAsync()
         {
-            var nico = new Person
-            {
-                id = "5",
-                name = "Nico",
-                passion = "Coden"
-            };
-            
-            Console.WriteLine("Works");
-            ItemResponse<Person> personResponse = await this.container.CreateItemAsync<Person>(nico, new PartitionKey(nico.name));
+            const string customerId = "1";
+            const string partitionKeyValue = "Germany";
+
+            // Delete an item. Note we must provide the partition key value and id of the item to delete
+            await this.customerContainer.DeleteItemAsync<Customer>(customerId, new PartitionKey(partitionKeyValue));
+            Console.WriteLine("Deleted Customer [{0},{1}]\n", customerId, partitionKeyValue);
         }
     }
 }
