46371582d3e409fe8c6d9dd9c5c45810f058e04a
gianmarcog <marco.giretti@web.de>
2019-12-01 14:15:35 +0100

    fix error

--- Cosmos-DB/Object/Reservation.cs
+++ Cosmos-DB/Object/Reservation.cs
@@ -9,13 +9,13 @@ namespace Cosmos_DB.Object
         public string id { get; set; }
         public string customer_id { get; set; }
         public string apartment_id { get; set; }
-        public DateTime booking_date { get; set; }
+        public DateTime? booking_date { get; set; }
         public string invoice_number { get; set; }
-        public string receipt_of_payment { get; set; }
-        public DateTime from { get; set; }
-        public DateTime to { get; set; }
+        public DateTime? receipt_of_payment { get; set; }
+        public DateTime? from { get; set; }
+        public DateTime? to { get; set; }
         public string type { get; set; }
-        public DateTime invoice_date { get; set; }
-        public double invoice_amount { get; set; }
+        public DateTime? invoice_date { get; set; }
+        public double? invoice_amount { get; set; }
     }
 }
