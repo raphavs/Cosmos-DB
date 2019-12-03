ea8b4af90bbd981c503173f7e261363a571d13a3
gianmarcog <marco.giretti@web.de>
2019-12-03 11:05:52 +0100

    added delete function

--- Cosmos-DB/DeleteReservation.cs
+++ Cosmos-DB/DeleteReservation.cs
@@ -5,9 +5,12 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
-using Cosmos_DB.HelpData;
+using Cosmos_DB.Help;
 using Cosmos_DB.Object;
 using Microsoft.Azure.Cosmos;
+using System.Threading.Tasks;
+
+
 
 namespace Cosmos_DB
 {
@@ -30,13 +33,33 @@ namespace Cosmos_DB
                 var currentResultSet = await queryResultSetIterator.ReadNextAsync();
                 foreach (var reservation in currentResultSet)
                 {
-                    Console.WriteLine("-------------------");
-                    Console.WriteLine(reservation.customer_id);
-                    Console.WriteLine("-------------------");
-                    Console.WriteLine();
+                    Console.WriteLine("-----------------------");
+                    Console.WriteLine("Customer " + reservation.customer_id);
+                    Console.WriteLine("Reservation " + reservation.apartment_id);
+                    Console.WriteLine("Booking Date " + reservation.booking_date);
+                    Console.WriteLine("From " + reservation.from);
+                    Console.WriteLine("To " + reservation.to);
+                    Console.WriteLine("Type " + reservation.type);
+                    Console.WriteLine("------------------------");
                 }
             } 
-        }
 
+                 DeleteReservationInput();
+        }
+                private void DeleteReservationInput()
+        {
+                Console.Write("ID input: ");
+                var customerId = Console.ReadLine();
+                Console.Write("Tyoe input: ");
+                var partitionKeyValue = Console.ReadLine();
+
+                DeleteReservationItemAsync(customerId,partitionKeyValue);
+
+            
+        }
+        private async Task DeleteReservationItemAsync(String customerId, String partitionKeyValue){
+            await this.reservationContainer.DeleteItemAsync<Reservation>(customerId, new PartitionKey(partitionKeyValue));
+            Console.WriteLine("Deleted Customer [{0},{1}]\n", customerId, partitionKeyValue);
+        }
     }
 }
