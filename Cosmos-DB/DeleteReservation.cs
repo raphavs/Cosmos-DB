e8a6214e57b0f6962344b99fd65340ec3e88b3f2
gianmarcog <marco.giretti@web.de>
2019-12-10 10:59:25 +0100

    make the delete usde case work

--- Cosmos-DB/DeleteReservation.cs
+++ Cosmos-DB/DeleteReservation.cs
@@ -9,31 +9,43 @@ using Cosmos_DB.Help;
 using Cosmos_DB.Object;
 using Microsoft.Azure.Cosmos;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Threading;
 
 
 
 namespace Cosmos_DB
 {
-    public class DeleteReservation{
+    public class DeleteReservation
+    {
 
         private readonly Container reservationContainer;
+        private readonly List<Reservation> reservations;
 
-        public DeleteReservation(Container container){
+
+        public DeleteReservation(Container container)
+        {
             this.reservationContainer = container;
+            this.reservations = new List<Reservation>();
         }
 
-        public async void Start(){
+        public async void Start()
+        {
             const string sqlQueryText = "SELECT * FROM c";
 
             var queryDefinition = new QueryDefinition(sqlQueryText);
-            var queryResultSetIterator = this.reservationContainer.GetItemQueryIterator<Reservation>(queryDefinition);      
+            var queryResultSetIterator = this.reservationContainer.GetItemQueryIterator<Reservation>(queryDefinition);
+            var index = 0;
 
             while (queryResultSetIterator.HasMoreResults)
             {
                 var currentResultSet = await queryResultSetIterator.ReadNextAsync();
+
                 foreach (var reservation in currentResultSet)
                 {
+                    index++;
                     Console.WriteLine("-----------------------");
+                    Console.WriteLine(index + ". Reservation");
                     Console.WriteLine("Customer " + reservation.customer_id);
                     Console.WriteLine("Reservation " + reservation.apartment_id);
                     Console.WriteLine("Booking Date " + reservation.booking_date);
@@ -41,25 +53,49 @@ namespace Cosmos_DB
                     Console.WriteLine("To " + reservation.to);
                     Console.WriteLine("Type " + reservation.type);
                     Console.WriteLine("------------------------");
+
+                    // Add reservationn
+                    reservations.Add(reservation);
                 }
-            } 
-
-                 DeleteReservationInput();
+            }
+            DeleteReservationInput();
         }
-                private void DeleteReservationInput()
+
+        private void DeleteReservationInput()
         {
-                Console.Write("ID input: ");
-                var customerId = Console.ReadLine();
-                Console.Write("Tyoe input: ");
-                var partitionKeyValue = Console.ReadLine();
+            var deleteIndex = -1;
 
-                DeleteReservationItemAsync(customerId,partitionKeyValue).GetAwaiter().GetResult();
+            while (deleteIndex < 1 || deleteIndex > reservations.Count)
+            {
 
-            
+                Console.Write("Please select the reservation by entering the corresponding number: ");
+
+                try
+                {
+                    deleteIndex = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Input is not valid!");
+                }
+            }
+            var selectedReservation = reservations.ElementAt(deleteIndex - 1);
+
+            //Console.Write("ID input: ");
+            //var customerId = Console.ReadLine();
+            //Console.Write("Type input: ");
+            //var partitionKeyValue = Console.ReadLine();
+            DeleteReservationItemAsync(selectedReservation).GetAwaiter().GetResult();
+
+            reservations.RemoveAt(deleteIndex);
         }
-        private async Task DeleteReservationItemAsync(String customerId, String partitionKeyValue){
-            await this.reservationContainer.DeleteItemAsync<Reservation>(customerId, new PartitionKey(partitionKeyValue));
-            Console.WriteLine("Deleted Customer [{0},{1}]\n", customerId, partitionKeyValue);
+
+
+
+        private async Task DeleteReservationItemAsync(Reservation reservation)
+        {
+            await this.reservationContainer.DeleteItemAsync<Reservation>(reservation.id, new PartitionKey(reservation.type));
+            Console.WriteLine("Deleted Customer [{0},{1}]\n", reservation.id, reservation.type);
         }
     }
 }
